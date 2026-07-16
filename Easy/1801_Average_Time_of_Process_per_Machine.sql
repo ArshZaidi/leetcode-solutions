59904bad2ce9cc911e1735beadddf47e9b5cda5b
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-07-16 18:29:49 +0530

    Sync LeetCode solution: Average Time of Process per Machine (MySQL)

---
+++ Easy/1801_Average_Time_of_Process_per_Machine.sql
@@ -0,0 +1,19 @@
+-- Problem: Average Time of Process per Machine
+-- Problem ID: 1801
+-- Difficulty: Easy
+-- Language: MySQL
+-- Runtime: 220 ms
+-- Memory: 0B
+-- Synced From: LeetCode
+-- Date: 2026-07-16
+
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id
+AND a.process_id = b.process_id
+WHERE a.activity_type = 'start'
+AND b.activity_type = 'end'
+GROUP BY a.machine_id;
