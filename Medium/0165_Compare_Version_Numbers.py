f2e23a3f5080e15ce5a180e4bc37f37b8db10d73
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-07-20 12:52:03 +0530

    Sync LeetCode solution: Compare Version Numbers (Python)

---
+++ Medium/0165_Compare_Version_Numbers.py
@@ -0,0 +1,26 @@
+# Problem: Compare Version Numbers
+# Problem ID: 165
+# Difficulty: Medium
+# Language: Python
+# Runtime: 0 ms
+# Memory: 12.2 MB
+# Synced From: LeetCode
+# Date: 2026-07-20
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        v1 = version1.split(".")
+        v2 = version2.split(".")
+
+        n = max(len(v1), len(v2))
+
+        for i in range(n):
+            num1 = int(v1[i]) if i < len(v1) else 0
+            num2 = int(v2[i]) if i < len(v2) else 0
+
+            if num1 > num2:
+                return 1
+            elif num1 < num2:
+                return -1
+
+        return 0
