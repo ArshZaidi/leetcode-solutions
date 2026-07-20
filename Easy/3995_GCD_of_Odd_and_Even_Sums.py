25b755c15398da637d635444d04e2e9994341708
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-07-20 14:11:52 +0530

    Sync LeetCode solution: GCD of Odd and Even Sums (Python)

---
+++ Easy/3995_GCD_of_Odd_and_Even_Sums.py
@@ -0,0 +1,13 @@
+# Problem: GCD of Odd and Even Sums
+# Problem ID: 3995
+# Difficulty: Easy
+# Language: Python
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# Synced From: LeetCode
+# Date: 2026-07-20
+
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        return n
+        
