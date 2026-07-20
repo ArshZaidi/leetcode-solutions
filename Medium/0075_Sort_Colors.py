e0d06216e94ea0c524c877eeabfb0414feab5817
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-07-20 11:37:31 +0530

    Sync LeetCode solution: Sort Colors (Python)

---
+++ Medium/0075_Sort_Colors.py
@@ -0,0 +1,13 @@
+# Problem: Sort Colors
+# Problem ID: 75
+# Difficulty: Medium
+# Language: Python
+# Runtime: 0 ms
+# Memory: 12.3 MB
+# Synced From: LeetCode
+# Date: 2026-07-20
+
+class Solution(object):
+    def sortColors(self, nums):
+        return nums.sort()
+        
