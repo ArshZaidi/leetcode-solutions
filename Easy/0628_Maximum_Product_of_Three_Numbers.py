63a0e044e2b0adba197e116d378ea9bb7193c8c7
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-07-26 12:14:22 +0530

    Sync LeetCode solution: Maximum Product of Three Numbers (Python3)

--- Easy/0628_Maximum_Product_of_Three_Numbers.py
+++ Easy/0628_Maximum_Product_of_Three_Numbers.py
@@ -1,14 +1,17 @@
 # Problem: Maximum Product of Three Numbers
 # Problem ID: 628
 # Difficulty: Easy
-# Language: Python
-# Runtime: 34 ms
-# Memory: 13.1 MB
+# Language: Python3
+# Runtime: 17 ms
+# Memory: 20.3 MB
 # Synced From: LeetCode
 # Date: 2026-07-26
 
-class Solution(object):
-    def maximumProduct(self, nums):
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
         nums.sort()
-        return max(nums[-1] * nums[-2] * nums[-3],
-                   nums[0] * nums[1] * nums[-1])
+        return max(
+            nums[-1] * nums[-2] * nums[-3],
+            nums[0] * nums[1] * nums[-1],
+            nums[0] * nums[1] * nums[2]
+        )
