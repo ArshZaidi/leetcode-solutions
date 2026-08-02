e3b46b1dcaa94f5e372b3f05042d267ecf1d3548
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-08-02 23:20:08 +0530

    Sync LeetCode solution: Stone Game (Python3)

---
+++ Medium/0909_Stone_Game.py
@@ -0,0 +1,12 @@
+# Problem: Stone Game
+# Problem ID: 909
+# Difficulty: Medium
+# Language: Python3
+# Runtime: 0 ms
+# Memory: 19.3 MB
+# Synced From: LeetCode
+# Date: 2026-08-02
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
