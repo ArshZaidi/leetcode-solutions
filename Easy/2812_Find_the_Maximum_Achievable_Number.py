87820122a0086679dd2d1901fc188520b1aec8a7
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-08-05 00:21:17 +0530

    Sync LeetCode solution: Find the Maximum Achievable Number (Python3)

---
+++ Easy/2812_Find_the_Maximum_Achievable_Number.py
@@ -0,0 +1,12 @@
+# Problem: Find the Maximum Achievable Number
+# Problem ID: 2812
+# Difficulty: Easy
+# Language: Python3
+# Runtime: 0 ms
+# Memory: 19.3 MB
+# Synced From: LeetCode
+# Date: 2026-08-04
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2 * t
