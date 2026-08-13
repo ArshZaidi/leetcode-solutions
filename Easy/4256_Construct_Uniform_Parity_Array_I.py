6bcca3bcd9685da6771a6eddae8d1a9edfaea728
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-08-13 16:38:32 +0530

    Sync LeetCode solution: Construct Uniform Parity Array I (Python3)

---
+++ Easy/4256_Construct_Uniform_Parity_Array_I.py
@@ -0,0 +1,12 @@
+# Problem: Construct Uniform Parity Array I
+# Problem ID: 4256
+# Difficulty: Easy
+# Language: Python3
+# Runtime: 0 ms
+# Memory: 19.4 MB
+# Synced From: LeetCode
+# Date: 2026-08-13
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
