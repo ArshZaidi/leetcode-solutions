70a708d7016feaa0879cbaa19a37b2cbe654c3c2
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-08-20 19:04:41 +0530

    Sync LeetCode solution: Distribute Elements Into Two Arrays I (Python3)

---
+++ Easy/3347_Distribute_Elements_Into_Two_Arrays_I.py
@@ -0,0 +1,22 @@
+# Problem: Distribute Elements Into Two Arrays I
+# Problem ID: 3347
+# Difficulty: Easy
+# Language: Python3
+# Runtime: 4 ms
+# Memory: 19.3 MB
+# Synced From: LeetCode
+# Date: 2026-08-20
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+
+        for i in range(2,len(nums)):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i]) 
+        
+        result = arr1 + arr2
+        return result
