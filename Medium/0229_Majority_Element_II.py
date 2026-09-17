1c1d8770bf202437455923e23b28db8ddecb6b50
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-09-17 22:48:34 +0530

    Sync LeetCode solution: Majority Element II (Python3)

---
+++ Medium/0229_Majority_Element_II.py
@@ -0,0 +1,42 @@
+# Problem: Majority Element II
+# Problem ID: 229
+# Difficulty: Medium
+# Language: Python3
+# Runtime: 1 ms
+# Memory: 22.3 MB
+# Synced From: LeetCode
+# Date: 2026-09-17
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        candidate1 = None
+        candidate2 = None
+        count1 = 0
+        count2 = 0
+
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+
+            elif num == candidate2:
+                count2 += 1
+
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        result = []
+
+        for candidate in [candidate1, candidate2]:
+            if candidate is not None and nums.count(candidate) > len(nums) // 3:
+                result.append(candidate)
+
+        return result
