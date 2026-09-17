392750776e58f4275de038eb9244a57802d69bae
Arsh Raza Zaidi <arshzaidi03@gmail.com>
2026-09-17 19:55:24 +0530

    Sync LeetCode solution: Reverse Bits (Python3)

---
+++ Easy/0190_Reverse_Bits.py
@@ -0,0 +1,18 @@
+# Problem: Reverse Bits
+# Problem ID: 190
+# Difficulty: Easy
+# Language: Python3
+# Runtime: 3 ms
+# Memory: 19.3 MB
+# Synced From: LeetCode
+# Date: 2026-09-17
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+
+        for _ in range(32):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+
+        return result
